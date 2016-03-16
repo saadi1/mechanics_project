3c72b1caa9d170f1b0ccf340dd57a40cbd86bb96
kirmaniarslan <arslankirmani9600@gmail.com>
2016-03-16 22:10:51 +0500

    Implement the formula
    Signed-off-by: kirmaniarslan <arslankirmani9600@gmail.com>

--- main_source.cpp
+++ main_source.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
+#include <math.h>
 #include "graphics.h"
+
+using namespace std;
+
+double gateEntry();            //returns the result from formula
+void gatePosition(int);        //will control gate position
 
 int main( )
 {
-    initwindow(400, 300, "Sample");
+	initwindow(400, 300, "Gate of Forbidden Palace");
+	
+	double value = 0;
+	/*
 	setcolor(YELLOW);
-    rectangle(20,20,100,100);
-    while (!kbhit( ))
-    {
-        delay(200);
-    }
-    return 0;
+	rectangle(20,20,100,100);
+	*/
+
+	value = gateEntry();	
+
+	while (!kbhit( ))
+	{
+		delay(200);
+	}
+	return 0;
+}
+
+double gateEntry() 
+{
+
+	const float PI = 3.14159265; 
+	const float num1 = 0.2, num2 = 0.4;
+	float value = 0; 
+	double formula = 0;
+
+	//take value of theeta
+	cout << "\nEnter the Value" << endl;
+	cin >> value;
+
+	//generates value of : M/|T|
+	formula = ( num1 * cos( value * PI / 180.0 ) ) / ( powf( (0.41 - num2 * sin( value * PI / 180.0 ) ), 0.5 ) );
+
+	cout << formula << endl;  //for debugging
+
+	return formula;
+	
 }
