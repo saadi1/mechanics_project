3202d2adb2211e9d4b1e5c8c7dbec363f50643a5
Saadi <batch15.026saad@gmail.com>
2016-04-10 22:15:31 +0500

    Update main_source.cpp
    Updated Cases

--- main_source.cpp
+++ main_source.cpp
@@ -4,52 +4,148 @@
 
 using namespace std;
 
-double gateEntry();            //returns the result from formula
-void gatePosition(int);        //will control gate position
+double gateEntry( int );            //returns the result from formula
+void gatePosition( double, int );        //will control gate position
 
 int main( )
 {
-	initwindow(400, 300, "Gate of Forbidden Palace");
-	
-	double value = 0;
-	/*
-	setcolor(YELLOW);
-	rectangle(20,20,100,100);
-	*/
 
-	value = gateEntry();	
+	double value = 0;
+	int angle = 0; 
+
+	//take value of theeta
+	cout << "\nEnter the Value" << endl;
+	cin >> angle;
+
+	value = gateEntry( angle );
+	if(angle>0&&angle<=10)
+		{
+			angle=10;
+	    }
+	if(angle>=11&&angle<=30)
+		{
+			angle=30;
+	    }
+	if(angle>=31&&angle<=50)
+		{
+			angle=50;
+	    }
+	if(angle>=51&&angle<=70)
+		{
+			angle=70;
+	    }
+	if(angle>=71&&angle<=89)
+		{
+			angle=85;
+	    }
+
+	gatePosition( value, angle );
 
 	while (!kbhit( ))
 	{
-		delay(200);
+		delay( 200 );
 	}
+
 	return 0;
 }
 
-double gateEntry() 
+double gateEntry( int angle ) 
 {
 
 	const float PI = 3.14159265; 
 	const float num1 = 0.2, num2 = 0.4;
-	float value = 0; 
 	double formula = 0;
 
-	//take value of theeta
-	cout << "\nEnter the Value" << endl;
-	cin >> value;
-	if(value==90)
-	{
-		formula=0;
-		cout<<formula;
+	if (angle == 90)
 		return 0;
-		
-	} // fix crash at angle 90
 
 	//generates value of : M/|T|
-	formula = ( num1 * cos( value * PI / 180.0 ) ) / ( powf( (0.41 - num2 * sin( value * PI / 180.0 ) ), 0.5 ) );
+	formula = ( num1 * cos( angle * PI / 180.0 ) ) / ( powf( (0.41 - num2 * sin( angle * PI / 180.0 ) ), 0.5 ) );
 
 	cout << formula << endl;  //for debugging
 
 	return formula;
-	
+
 }
+
+void gatePosition( double value, int angle )
+{
+
+	initwindow(600, 550, "Gate of Forbidden Palace");
+
+	setcolor( WHITE );
+	
+
+	switch(angle)
+	{
+		
+
+	case 10:
+		{
+
+		outtextxy(350,450,"angle 10");
+		getch();
+		exit(0);
+		}// case 1 ends
+		
+		
+		
+	case 30:
+		{
+			outtextxy(450,350,"angle 30");
+			getch();	
+			exit(0);
+		
+		
+		}// case 2 ends
+		
+	
+	case 50:
+		{
+			outtextxy(250,350,"angle 50");
+		getch();
+			exit(0);
+		}// case 3 ends
+		
+	
+	case 70:
+		{
+			outtextxy(250,350,"angle 70");
+			getch();
+			exit(0);
+		
+		}// case 4 ends
+	
+	case 85:
+		{
+			outtextxy(250,350,"angle 85");
+			getch();
+			exit(0);
+		
+		}// case 5 ends
+		
+		
+		
+
+	
+	case 90:
+		{
+		
+		
+		
+		
+		}
+	
+	
+	}
+
+
+
+
+	
+	} // switch ends
+	
+	
+	
+
+
